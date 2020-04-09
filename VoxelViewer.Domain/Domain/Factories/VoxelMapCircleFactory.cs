4da12e0d8236faa57e32f8a09cd6884226a7e36f
Denis535 <dddenisss35@gmail.com>
2020-04-09 15:13:54 +0300

    Refactoring

--- VoxelViewer.Domain/Domain/Factories/VoxelMapCircleFactory.cs
+++ VoxelViewer.Domain/Domain/Factories/VoxelMapCircleFactory.cs
@@ -15,6 +15,9 @@
         public static VoxelMap Create(int width, int height, int cx, int cy, int radius) {
             var cells = EnumerableEx.GetIterator2D( width, height )
                 .Select( i => GetValue( i.X, i.Y, cx, cy, radius ) )
+                .Select( i => i.Pow( 1.5f ) )
+                .Select( i => i.WithRandom( 0.3f ) )
+                .Select( i => i.ToInt() )
                 .Select( i => (VoxelCell) i )
                 .ToArray2D( width, height );
             return new VoxelMap( width, height, cells );
@@ -22,18 +25,13 @@
 
 
         // Helpers/Value
-        private static int GetValue(int x, int y, int cx, int cy, int radius) {
-            return GetDistance( x, y, cx, cy )
-                .Normalize( 0, radius )
-                .ToInt()
-                .Invert()
-                .Clamp()
-                .WithRandom( 150 );
+        private static float GetValue(int x, int y, int cx, int cy, int radius) {
+            return GetDistance( x, y, cx, cy ).Normalize( 0, radius ).Clamp().Invert();
         }
 
         // Helpers/Math
-        private static int GetDistance(int x, int y, int x2, int y2) {
-            return (int) Math.Sqrt( GetSqrDistance( x, y, x2, y2 ) );
+        private static float GetDistance(int x, int y, int x2, int y2) {
+            return (float) Math.Sqrt( GetSqrDistance( x, y, x2, y2 ) );
         }
         //private static int GetDistance(int x, int y, int x2, int y2) {
         //    var dx = Math.Abs( x - x2 );
@@ -45,24 +43,27 @@
             var dy = Math.Abs( y - y2 );
             return (dx * dx) + (dy * dy);
         }
-        //private static int Normalize(this int value, int from, int to) {
-        //    return value * to / from; // from 0..from to 0..to (value / from * to)
-        //}
-        private static float Normalize(this int value, int min, int max) {
-            return (float) (value - min) / (max - min);
+        private static float Normalize(this float value, float min, float max) {
+            return (value - min) / (max - min);
+        }
+        private static float Clamp(this float value) {
+            value = Math.Min( value, 1 );
+            value = Math.Max( value, 0 );
+            return value;
+        }
+        private static float Invert(this float value) {
+            return 1 - value;
+        }
+        private static float Pow(this float value, float power) {
+            return (float) Math.Pow( value, power );
         }
         private static int ToInt(this float value) {
             return (int) (value * VoxelCell.MaxValue);
         }
-        private static int Invert(this int value) {
-            return VoxelCell.MaxValue - value;
-        }
-        private static int Clamp(this int value) {
-            return Math.Max( value, 0 );
-        }
-        private static int WithRandom(this int value, int max) {
+        // Helpers/Random
+        private static float WithRandom(this float value, float max) {
             var invValue = Math.Max( max - value, 0 );
-            value -= Random.Next( invValue );
+            value -= (float) Random.NextDouble() * invValue;
             return Math.Max( value, 0 );
         }
 
--- VoxelViewer.Domain/Domain/Factories/VoxelMapNoiseFactory.cs
+++ VoxelViewer.Domain/Domain/Factories/VoxelMapNoiseFactory.cs
@@ -15,6 +15,7 @@
                 .Select( i => i.Staircase( 1f / 15 ) )
                 .Select( i => i.Pow( 1.5f ) )
                 .Select( i => i.ToInt() )
+                //.Mix()
                 .Select( i => (VoxelCell) i )
                 .ToArray2D( width, height );
             return new VoxelMap( width, height, cells );
@@ -48,6 +49,27 @@
         }
         private static int ToInt(this float value) {
             return (int) (value * VoxelCell.MaxValue);
+        }
+        // Helpers/Mix
+        private static IEnumerable<int> Mix(this IEnumerable<int> values) {
+            var table = CreateRandomTable();
+            foreach (var item in values) {
+                yield return table[ item ];
+            }
+        }
+        private static int[] CreateRandomTable() {
+            var rnd = new Random();
+            var table = Enumerable.Range( 0, 256 ).ToArray();
+            for (var i = 0; i < table.Length; i++) {
+                var i2 = rnd.Next( table.Length );
+                Swap( ref table[ i ], ref table[ i2 ] );
+            }
+            return table;
+        }
+        private static void Swap<T>(ref T a, ref T b) {
+            var tmp = a;
+            a = b;
+            b = tmp;
         }
 
 
